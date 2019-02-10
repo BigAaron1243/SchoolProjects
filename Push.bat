aa0412104017b3e1b7f3e2782110f0e2d0e44e95
Oscar <oscar.coghlan@gmail.com>
2019-02-11 08:14:54 +1100

    ftbgyn"

--- Push.bat
+++ Push.bat
@@ -3,14 +3,17 @@ dir /b
 echo .....
 echo What file(s) would you like to push?
 set /p files=
+echo Write a push message
+set /p message=
 echo Pushing %files%, are you sure (y/n)
 set /p yn=
 if %yn% == y goto y
 if %yn% == Y goto y
 if %yn% == n exit
 if %yn% == N exit
+exit
 :y
 git add %files%
-git commit -m "Push via app"
+git commit -m "%message%"
 git push origin master
 timeout 5 >NUL
