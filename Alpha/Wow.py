d5723d43f59377886c4ec8c77d4248ceba8ad1b4
Oscar <oscar.coghlan@gmail.com>
2019-02-12 07:14:22 +1100

    No

--- Alpha/Wow.py
+++ Alpha/Wow.py
@@ -1,6 +1,7 @@
-with open('wowow.txt', 'r') as myfile:
-    penis = None
-    gay = input("Choose operator: ")
-    data = "penis = " + myfile.read().replace('\n', gay)
-    exec(data)
-    print(penis)
+while True:
+    with open('wowow.txt', 'r') as myfile:
+        penis = None
+        gay = input("Choose operator: ")
+        data = "penis = " + myfile.read().replace('\n', gay)
+        exec(data)
+        print(penis)
